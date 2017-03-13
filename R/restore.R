42a9b12f35c90f2be94bbb98b02296b626d63fc0
Ncalverley2 <Ncalverley2@gmail.com>
2017-03-13 17:24:29 -0600

    Manually removing the XML package from any restore actions.

--- R/restore.R
+++ R/restore.R
@@ -620,6 +620,7 @@ restoreImpl <- function(project,
     )
   actions <- diff(installedPkgRecords, pkgRecords)
   actions[names(actions) %in% pkgsToIgnore] <- NA
+  actions[names(actions) == "XML"] <- NA
   restartNeeded <- FALSE
 
   mustConfirm <- any(c('downgrade', 'remove', 'crossgrade') %in% actions)
